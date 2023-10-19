06f42bc8dc241947e6913aa583967e45c2a774e2
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-10-19 10:26:02 +0000

    Add metadata and cleanup code

--- index.js
+++ index.js
@@ -1,76 +1,112 @@
-"use strict";
+'use strict'
 
-const fs = require("fs");
-const csv = require("fast-csv");
-const env = require("env-var");
-const mqtt = require("mqtt");
+const fs = require('fs')
+const csv = require('fast-csv')
+const env = require('env-var')
+const mqtt = require('mqtt')
+const crypto = require('crypto')
 
-const CsvFile = env.get("CSV_FILE").required().asString();
-const MqttUrl = env.get("MQTT_URL").required().asString();
-const MqttUsername = env.get("MQTT_USERNAME").asString();
-const MqttPassword = env.get("MQTT_PASSWORD").asString();
+const CsvFile = env.get('CSV_FILE').required().asString()
+const MqttUrl = env.get('MQTT_URL').required().asString()
+const MqttUsername = env.get('MQTT_USERNAME').asString()
+const MqttPassword = env.get('MQTT_PASSWORD').asString()
+const ThingModel = env.get('THING_MODEL').required().default('https://raw.githubusercontent.com/salberternst/thing-models/main/home_assistant/power.json').asString()
 
 // returns the milliseconds of the day
-function getMillisecondsOfDay(date) {
+function getMillisecondsOfDay (date) {
   return (
     (date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds()) *
       1000 +
     date.getMilliseconds()
-  );
+  )
 }
 
-function sleep(ms) {
+function sleep (ms) {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+    setTimeout(resolve, ms)
+  })
 }
 
-async function run() {
+function getUniqueDeviceId (name) {
+  return crypto.createHash('sha1').update(`${name}:${MqttUsername}`).digest('hex')
+}
+
+function sendAttributes (mqttClient, row) {
+  const attributes = {}
+  for (const key of Object.keys(row)) {
+    if (key === 'timestamp') continue
+    const deviceId = getUniqueDeviceId(key)
+    attributes[deviceId] = {
+      'thing-model': ThingModel,
+      'thing-metadata': {
+        description: key
+      }
+    }
+  }
+
+  mqttClient.publish('v1/gateway/attributes', JSON.stringify(attributes))
+}
+
+function sendTelemetry (mqttClient, row) {
+  const telemetry = {}
+  for (const key of Object.keys(row)) {
+    if (key === 'timestamp') continue
+    const deviceId = getUniqueDeviceId(key)
+    telemetry[deviceId] = [
+      {
+        power: +row[key]
+      }
+    ]
+  }
+  mqttClient.publish('v1/gateway/telemetry', JSON.stringify(telemetry))
+}
+
+async function run () {
   const mqttClient = mqtt.connect(MqttUrl, {
     username: MqttUsername,
-    password: MqttPassword,
-  });
+    password: MqttPassword
+  })
 
-  mqttClient.on("connect", () => {
+  mqttClient.on('message', (topic, message) => {
+    console.log(topic, message)
+  })
+
+  mqttClient.on('connect', () => {
+    let sentAttributes = false
     const csvOptions = {
       headers: true,
-      delimiter: ",",
-    };
-    const readable = fs.createReadStream(CsvFile)
+      delimiter: ','
+    }
+    const readable = fs
+      .createReadStream(CsvFile)
       .pipe(csv.parse(csvOptions))
-      .on("error", (error) => console.error(error))
-      .on("data", async (row) => {
-        const now = getMillisecondsOfDay(new Date());
-        const time = getMillisecondsOfDay(
-          new Date(row.timestamp)
-        );
-        const diff = time - now;
+      .on('error', (error) => console.error(error))
+      .on('data', async (row) => {
+        const now = getMillisecondsOfDay(new Date())
+        const time = getMillisecondsOfDay(new Date(row.timestamp))
+        const diff = time - now
         if (diff >= 0) {
           readable.pause()
           await sleep(diff)
 
-          const telemetry = {}          
-          for (const key of Object.keys(row)) {
-            if (key === "timestamp") continue;
-            const name = key.replace(" ", "_");
-            telemetry[name] = [{
-              power: +row[key]
-            }]
+          if (sentAttributes === false) {
+            sendAttributes(mqttClient, row)
+            sentAttributes = true
           }
 
-          mqttClient.publish('v1/gateway/telemetry', JSON.stringify(telemetry))
+          sendTelemetry(mqttClient, row)
 
           readable.resume()
         }
       })
-  });
+  })
 
-  mqttClient.on("error", (err) => {
-    console.error(err);
-    process.exit(1);
-  });
+  mqttClient.on('error', (err) => {
+    console.error(err)
+    process.exit(1)
+  })
 }
 
 run()
   .then(() => console.log(`Replaying ${CsvFile}`))
-  .catch((e) => console.error(e));
+  .catch((e) => console.error(e))
