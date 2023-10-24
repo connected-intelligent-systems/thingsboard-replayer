e8b20c788f9203e2e821a27b200c938e80dda092
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-10-24 07:30:44 +0000

    Add support for refit data

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
+FROM alpine:latest AS refit
+
+RUN apk --update --no-cache add wget 7zip && rm -rf /var/cache/apk/*
+
+WORKDIR /refit
+
+RUN wget https://pureportal.strath.ac.uk/files/52873459/Processed_Data_CSV.7z && \
+    7z x Processed_Data_CSV.7z && \
+    rm Processed_Data_CSV.7z
+
 FROM node:12.21.0
 
 WORKDIR /app
@@ -5,6 +15,7 @@ WORKDIR /app
 COPY package.json /app
 COPY package-lock.json /app
 COPY example_data /app/example_data
+COPY --from=refit /refit/*.csv /app/example_data
 
 RUN npm install --production
 
--- index.js
+++ index.js
@@ -6,11 +6,41 @@ const env = require('env-var')
 const mqtt = require('mqtt')
 const crypto = require('crypto')
 
+const CsvTimestampFormats = Object.freeze({
+  ISO: 'ISO',
+  TS: 'TS'
+})
+
 const CsvFile = env.get('CSV_FILE').required().asString()
+const CsvTimestampColumn = env
+  .get('CSV_TIMESTAMP_COLUMN')
+  .default('Time')
+  .required()
+  .asString()
+const CsvTimestampFormat = env
+  .get('CSV_TIMESTAMP_FORMAT')
+  .default(CsvTimestampFormats.ISO)
+  .required()
+  .asEnum([CsvTimestampFormats.ISO, CsvTimestampFormats.TS])
+const CsvIgnoreColumns = env
+  .get('CSV_IGNORE_COLUMNS')
+  .default('Unix')
+  .asArray()
 const MqttUrl = env.get('MQTT_URL').required().asString()
 const MqttUsername = env.get('MQTT_USERNAME').asString()
 const MqttPassword = env.get('MQTT_PASSWORD').asString()
-const ThingModel = env.get('THING_MODEL').required().default('https://raw.githubusercontent.com/salberternst/thing-models/main/home_assistant/power.json').asString()
+const ThingModel = env
+  .get('THING_MODEL')
+  .required()
+  .default(
+    'https://raw.githubusercontent.com/salberternst/thing-models/main/home_assistant/power.json'
+  )
+  .asString()
+const MaxWaitTime = env
+  .get('MAX_WAIT_TIME')
+  .required()
+  .default('60000')
+  .asIntPositive()
 
 // returns the milliseconds of the day
 function getMillisecondsOfDay (date) {
@@ -28,13 +58,16 @@ function sleep (ms) {
 }
 
 function getUniqueDeviceId (name) {
-  return crypto.createHash('sha1').update(`${name}:${MqttUsername}`).digest('hex')
+  return crypto
+    .createHash('sha1')
+    .update(`${name}:${MqttUsername}`)
+    .digest('hex')
 }
 
 function sendAttributes (mqttClient, row) {
   const attributes = {}
   for (const key of Object.keys(row)) {
-    if (key === 'timestamp') continue
+    if (key === CsvTimestampColumn || CsvIgnoreColumns.includes(key)) continue
     const deviceId = getUniqueDeviceId(key)
     attributes[deviceId] = {
       'thing-model': ThingModel,
@@ -50,7 +83,7 @@ function sendAttributes (mqttClient, row) {
 function sendTelemetry (mqttClient, row) {
   const telemetry = {}
   for (const key of Object.keys(row)) {
-    if (key === 'timestamp') continue
+    if (key === CsvTimestampColumn || CsvIgnoreColumns.includes(key)) continue
     const deviceId = getUniqueDeviceId(key)
     telemetry[deviceId] = [
       {
@@ -61,14 +94,23 @@ function sendTelemetry (mqttClient, row) {
   mqttClient.publish('v1/gateway/telemetry', JSON.stringify(telemetry))
 }
 
+function getDate (row) {
+  const date = row[CsvTimestampColumn]
+  if (date === undefined) {
+    throw new Error('Invalid timestamp column')
+  }
+
+  if (CsvTimestampFormat === CsvTimestampFormats.ISO) {
+    return new Date(date)
+  } else {
+    return new Date(+date)
+  }
+}
+
 async function run () {
   const mqttClient = mqtt.connect(MqttUrl, {
     username: MqttUsername,
     password: MqttPassword
-  })
-
-  mqttClient.on('message', (topic, message) => {
-    console.log(topic, message)
   })
 
   mqttClient.on('connect', () => {
@@ -83,10 +125,11 @@ async function run () {
       .on('error', (error) => console.error(error))
       .on('data', async (row) => {
         const now = getMillisecondsOfDay(new Date())
-        const time = getMillisecondsOfDay(new Date(row.timestamp))
+        const time = getMillisecondsOfDay(getDate(row))
         const diff = time - now
-        if (diff >= 0) {
+        if (diff >= 0 && diff < MaxWaitTime) {
           readable.pause()
+
           await sleep(diff)
 
           if (sentAttributes === false) {
